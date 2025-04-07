9d6de04cb028906a2417b437e7d08eacbd3e3912
PJarb <w.pachinee@gmail.com>
2025-04-07 16:14:37 +0700

    Update chatdb.py

--- chatdb.py
+++ chatdb.py
@@ -1,6 +1,2 @@
 !pip install -q -U google-generativeai
 import google.generativeai as genai
-genai.configure(api_key='AIzaSyBdf4nh_jx7Jq3M8lZ4Zbfim6GULaNr9iI')
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(model.name)
